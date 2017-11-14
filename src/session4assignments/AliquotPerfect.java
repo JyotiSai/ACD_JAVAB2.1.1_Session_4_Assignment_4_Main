1727664cddb836538954966597959c51c73236b1
JyotiSai <kajal.jojo@gmail.com>
2017-11-14 12:58:54 +0530

    Update AliquotPerfect.java

--- src/session4assignments/AliquotPerfect.java
+++ src/session4assignments/AliquotPerfect.java
@@ -1,14 +1,15 @@
 package session4assignments; //Package Declaration
+
 /**
- * This class finds the aliquot sum of a given number, checks if it is a perfect number and
- * prints all perfect numbers between 1 and 100
+ * This class finds the aliquot sum of a given number, checks if it is a perfect
+ * number and prints all perfect numbers between 1 and 100
  *
  */
 public class AliquotPerfect 
 {
 	/**
-	 * This is the main method which accepts command line arguments and
-	 * returns no value
+	 * This is the main method which accepts command line arguments and returns no
+	 * value
 	 *
 	 */
 	public static void main(String[] args) 
@@ -17,17 +18,17 @@ public class AliquotPerfect
 
 		for (int number = 1; number <= 100; number++) // Outer for loop for the given number from 1 to 100
 		{
-			int aliquotSum = 0; // Initialization of Aliquot sum variable
+			int aliquot_sum = 0; // Initialization of Aliquot sum variable
 
 			for (int divisor = 1; divisor != number; divisor++) // Inner for loop for dividing the given number to find aliquot divisors and sum
 			{
 				if ((number % divisor) == 0) // If condition using modulo operation to check if the remainder is 0
 				{
-					aliquotSum = aliquotSum + divisor; // Sum of aliquot divisors stored in aliquotSum variable
+					aliquot_sum = aliquot_sum + divisor; // Sum of aliquot divisors stored in aliquotSum variable
 				} // if block closed
 			} // Inner for loop closed
 
-			if (aliquotSum == number) // if condition to compare the equality of aliquot sum and the given number
+			if (aliquot_sum == number) // if condition to compare the equality of aliquot sum and the given number
 			{
 				System.out.println(number); // Prints the Perfect number if the above condition is true
 			} // if block closed
